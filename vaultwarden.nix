1eea85105ef12a856b07b4b94178b772ec481bc1
impulseSecDev <148917681+impulseSecDev@users.noreply.github.com>
2026-05-12 20:52:00 -0400

    Add extended logging and configuration options

--- vaultwarden.nix
+++ vaultwarden.nix
@@ -3,33 +3,71 @@
   sops.secrets = {
     "vw_admin_token" = {};
     "vw_domain" = {};
-  };  
+  };
 
   sops.templates."vaultwarden.env" = {
     content = ''
       ADMIN_TOKEN=${config.sops.placeholder."vw_admin_token"}
       DOMAIN=${config.sops.placeholder."vw_domain"}
+      EXTENDED_LOGGING=true
+      LOG_LEVEL=info
+      LOG_FILE=/var/lib/vaultwarden/vaultwarden.log
+      ORG_EVENTS_ENABLED=true
     '';
     path = "/run/secrets/vaultwarden.env";
     mode = "0400";
     owner = "root";
     group = "root";
-  };  
+  };
 
 
   services.vaultwarden = {
     enable = true;
     backupDir = "/var/local/vaultwarden/backup";
-    # in order to avoid having  ADMIN_TOKEN in the nix store it can be also set with the help of an environment file
-    # be aware that this file must be created by hand (or via secrets management like sops)
     environmentFile = [ config.sops.templates."vaultwarden.env".path ];
     config = {
-      # Refer to https://github.com/dani-garcia/vaultwarden/blob/main/.env.template
       SIGNUPS_ALLOWED = false;
 
       ROCKET_ADDRESS = "127.0.0.1";
       ROCKET_PORT = 8222;
       ROCKET_LOG = "critical";
     };
-  };  
+  };
+}
+cat: cat: No such file or directory
+{ config, lib, pkgs, ... }:
+{
+  sops.secrets = {
+    "vw_admin_token" = {};
+    "vw_domain" = {};
+  };
+
+  sops.templates."vaultwarden.env" = {
+    content = ''
+      ADMIN_TOKEN=${config.sops.placeholder."vw_admin_token"}
+      DOMAIN=${config.sops.placeholder."vw_domain"}
+      EXTENDED_LOGGING=true
+      LOG_LEVEL=info
+      LOG_FILE=/var/lib/vaultwarden/vaultwarden.log
+      ORG_EVENTS_ENABLED=true
+    '';
+    path = "/run/secrets/vaultwarden.env";
+    mode = "0400";
+    owner = "root";
+    group = "root";
+  };
+
+
+  services.vaultwarden = {
+    enable = true;
+    backupDir = "/var/local/vaultwarden/backup";
+    environmentFile = [ config.sops.templates."vaultwarden.env".path ];
+    config = {
+      SIGNUPS_ALLOWED = false;
+
+      ROCKET_ADDRESS = "127.0.0.1";
+      ROCKET_PORT = 8222;
+      ROCKET_LOG = "critical";
+    };
+  };
 }

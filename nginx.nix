7ce6113ee3ef694052ffa549a167e78b1e31e02e
tim <timothy.harris0187@gmail.com>
2026-06-21 21:07:12 -0400

    updated nginx hardening

--- nginx.nix
+++ nginx.nix
@@ -51,7 +51,7 @@
         listen = [ { addr = "100.64.0.5"; port = 80; } ];
         extraConfig = ''
           server_name _;
-          error_page 400 =444 /;
+          error_page 400 401 402 403 404 405 429 497 500 =444 /;
           return 444;
         '';
       };
